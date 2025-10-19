15d77a31d163992b2656ba4c4456ef02640578ce
P1LeFR <plstable@protonmail.com>
2025-10-19 16:04:36 +0200

    Update stage2.sh

--- builder/stage2.sh
+++ builder/stage2.sh
@@ -1,45 +1,58 @@
 #!/bin/bash
 set -eux
 
-# Chores
+# ---------------------------------------------------------------------------
+# Raccourcir le chemin pour éviter "Filename too long" lors des git clone
+# (fonctionne sur GitHub Actions Windows avec bash/git-bash)
+# ---------------------------------------------------------------------------
 git config --global core.autocrlf true
-git config --global core.longpaths true   # éviter les erreurs de chemins trop longs sous Windows
-workdir=$(pwd)
+git config --global core.longpaths true
+
+workdir="$(pwd)"
+# Chemin Windows (D:\a\...) depuis git-bash
+workdir_win="$(pwd -W)"
+# Monte un lecteur court W: vers le workspace courant
+cmd.exe /C subst W: "$workdir_win"
+short_root="W:/"
+
+# Utiliser W:\... comme racine pour toutes les opérations suivantes
+cd "$short_root"
+
 gcs='git clone --depth=1 --no-tags --recurse-submodules --shallow-submodules'
-export PYTHONPYCACHEPREFIX="$workdir/pycache2"
-export PATH="$PATH:$workdir/ComfyUI_Windows_portable/python_standalone/Scripts"
+export PYTHONPYCACHEPREFIX="$short_root/pycache2"
+export PATH="$PATH:$short_root/ComfyUI_Windows_portable/python_standalone/Scripts"
 
 ls -lahF
 
 # Redirect HuggingFace-Hub model folder
-export HF_HUB_CACHE="$workdir/ComfyUI_Windows_portable/HuggingFaceHub"
+export HF_HUB_CACHE="$short_root/ComfyUI_Windows_portable/HuggingFaceHub"
 mkdir -p "${HF_HUB_CACHE}"
 # Redirect Pytorch Hub model folder
-export TORCH_HOME="$workdir/ComfyUI_Windows_portable/TorchHome"
+export TORCH_HOME="$short_root/ComfyUI_Windows_portable/TorchHome"
 mkdir -p "${TORCH_HOME}"
 
-# Relocate python_standalone
-mv  "$workdir"/python_standalone  "$workdir"/ComfyUI_Windows_portable/python_standalone
+# Relocate python_standalone (déplacé dans le paquet portable)
+mv "$short_root/python_standalone" "$short_root/ComfyUI_Windows_portable/python_standalone"
 
 # Add MinGit (Portable Git)
 curl -sSL https://github.com/git-for-windows/git/releases/download/v2.50.1.windows.1/MinGit-2.50.1-64-bit.zip \
     -o MinGit.zip
-unzip -q MinGit.zip -d "$workdir"/ComfyUI_Windows_portable/MinGit
+unzip -q MinGit.zip -d "$short_root/ComfyUI_Windows_portable/MinGit"
 rm MinGit.zip
 
 ################################################################################
 # ComfyUI main app
 git clone https://github.com/comfyanonymous/ComfyUI.git \
-    "$workdir"/ComfyUI_Windows_portable/ComfyUI
+    "$short_root/ComfyUI_Windows_portable/ComfyUI"
 # Use latest stable version (has a release tag)
-cd "$workdir"/ComfyUI_Windows_portable/ComfyUI
+cd "$short_root/ComfyUI_Windows_portable/ComfyUI"
 git reset --hard "$(git tag | grep -e '^v' | sort -V | tail -1)"
 # Clear models folder (will restore in the next stage)
 rm -vrf models
 mkdir models
 
 # Custom Nodes
-cd "$workdir"/ComfyUI_Windows_portable/ComfyUI/custom_nodes
+cd "$short_root/ComfyUI_Windows_portable/ComfyUI/custom_nodes"
 $gcs https://github.com/Comfy-Org/ComfyUI-Manager.git
 
 # Workspace
@@ -69,7 +82,7 @@ $gcs https://github.com/Derfuu/Derfuu_ComfyUI_ModdedNodes.git          # Derfuu_
 # Control
 $gcs https://github.com/chflame163/ComfyUI_LayerStyle.git
 $gcs https://github.com/Fannovel16/comfyui_controlnet_aux.git
-$gcs https://codeberg.org/Gourieff/comfyui-reactor-node.git ComfyUI-ReActor      # <-- ReActor (nouveau dépôt)
+$gcs https://codeberg.org/Gourieff/comfyui-reactor-node.git ComfyUI-ReActor   # ReActor (Codeberg) -> dossier conservé
 $gcs https://github.com/florestefano1975/comfyui-portrait-master.git
 $gcs https://github.com/huchenlei/ComfyUI-IC-Light-Native.git
 $gcs https://github.com/huchenlei/ComfyUI-layerdiffuse.git
@@ -79,7 +92,7 @@ $gcs https://github.com/ltdrdata/ComfyUI-Impact-Pack.git
 $gcs https://github.com/ltdrdata/ComfyUI-Impact-Subpack.git
 $gcs https://github.com/ltdrdata/ComfyUI-Inspire-Pack.git
 $gcs https://github.com/mcmonkeyprojects/sd-dynamic-thresholding.git
-$gcs https://github.com/twri/sdxl_prompt_styler.gitnon
+$gcs https://github.com/twri/sdxl_prompt_styler.git
 
 # >>> Ajouts manquants - Control
 $gcs https://github.com/chflame163/ComfyUI_LayerStyle_Advance.git      # ComfyUI_LayerStyle_Advance
@@ -120,22 +133,22 @@ $gcs https://github.com/Suzie1/ComfyUI_Comfyroll_CustomNodes.git
 
 ################################################################################
 # Copy attachments files (incl. start scripts)
-cp -rf "$workdir"/attachments/. \
-    "$workdir"/ComfyUI_Windows_portable/
+cp -rf "$short_root/attachments/." \
+      "$short_root/ComfyUI_Windows_portable/"
 
-du -hd2 "$workdir"/ComfyUI_Windows_portable
+du -hd2 "$short_root/ComfyUI_Windows_portable"
 
 ################################################################################
 # TAESD model for image on-the-fly preview
-cd "$workdir"
+cd "$short_root"
 $gcs https://github.com/madebyollin/taesd.git
-mkdir -p "$workdir"/ComfyUI_Windows_portable/ComfyUI/models/vae_approx
+mkdir -p "$short_root/ComfyUI_Windows_portable/ComfyUI/models/vae_approx"
 cp taesd/*_decoder.pth \
-    "$workdir"/ComfyUI_Windows_portable/ComfyUI/models/vae_approx/
+   "$short_root/ComfyUI_Windows_portable/ComfyUI/models/vae_approx/"
 rm -rf taesd
 
 # Download models for ReActor
-cd "$workdir"/ComfyUI_Windows_portable/ComfyUI/models
+cd "$short_root/ComfyUI_Windows_portable/ComfyUI/models"
 curl -sSL https://github.com/sczhou/CodeFormer/releases/download/v0.1.0/codeformer.pth \
     --create-dirs -o facerestore_models/codeformer-v0.1.0.pth
 curl -sSL https://github.com/TencentARC/GFPGAN/releases/download/v1.3.4/GFPGANv1.4.pth \
@@ -152,31 +165,37 @@ curl -sSL https://huggingface.co/AdamCodd/vit-base-nsfw-detector/resolve/main/pr
     --create-dirs -o nsfw_detector/vit-base-nsfw-detector/preprocessor_config.json
 
 # Download models for Impact-Pack & Impact-Subpack
-cd "$workdir"/ComfyUI_Windows_portable/ComfyUI/custom_nodes/ComfyUI-Impact-Pack
-"$workdir"/ComfyUI_Windows_portable/python_standalone/python.exe -s -B install.py
-cd "$workdir"/ComfyUI_Windows_portable/ComfyUI/custom_nodes/ComfyUI-Impact-Subpack
-"$workdir"/ComfyUI_Windows_portable/python_standalone/python.exe -s -B install.py
+cd "$short_root/ComfyUI_Windows_portable/ComfyUI/custom_nodes/ComfyUI-Impact-Pack"
+"$short_root/ComfyUI_Windows_portable/python_standalone/python.exe" -s -B install.py
+cd "$short_root/ComfyUI_Windows_portable/ComfyUI/custom_nodes/ComfyUI-Impact-Subpack"
+"$short_root/ComfyUI_Windows_portable/python_standalone/python.exe" -s -B install.py
 
 ################################################################################
 # Run the test (CPU only), also let custom nodes download some models
-cd "$workdir"/ComfyUI_Windows_portable
+cd "$short_root/ComfyUI_Windows_portable"
 ./python_standalone/python.exe -s -B ComfyUI/main.py --quick-test-for-ci --cpu
 
 ################################################################################
 # Clean up
-rm -vf "$workdir"/ComfyUI_Windows_portable/*.log
-rm -vf "$workdir"/ComfyUI_Windows_portable/ComfyUI/user/*.log
-rm -vrf "$workdir"/ComfyUI_Windows_portable/ComfyUI/user/default/ComfyUI-Manager
+rm -vf "$short_root/ComfyUI_Windows_portable/"*.log
+rm -vf "$short_root/ComfyUI_Windows_portable/ComfyUI/user/"*.log
+rm -vrf "$short_root/ComfyUI_Windows_portable/ComfyUI/user/default/ComfyUI-Manager"
 
-cd "$workdir"/ComfyUI_Windows_portable/ComfyUI/custom_nodes
+cd "$short_root/ComfyUI_Windows_portable/ComfyUI/custom_nodes"
 rm -vf ./ComfyUI-Custom-Scripts/pysssss.json
 rm -vf ./ComfyUI-Easy-Use/config.yaml
 rm -vf ./ComfyUI-Impact-Pack/impact-pack.ini
 rm -vf ./Jovimetrix/web/config.json
 rm -vf ./was-node-suite-comfyui/was_suite_config.json
 
-cd "$workdir"/ComfyUI_Windows_portable/ComfyUI/custom_nodes/ComfyUI-Manager
+cd "$short_root/ComfyUI_Windows_portable/ComfyUI/custom_nodes/ComfyUI-Manager"
 git reset --hard
 git clean -fxd
 
+# ---------------------------------------------------------------------------
+# Démonte le lecteur virtuel W:
+# ---------------------------------------------------------------------------
+cmd.exe /C subst W: /D
+
+# Revenir au répertoire d’origine (par sûreté)
 cd "$workdir"
