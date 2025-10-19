a928d9bd1c1eac32df4c2613f7d5d0c671f88892
P1LeFR <plstable@protonmail.com>
2025-10-19 16:09:11 +0200

    Update generate-pak7.sh

--- builder/generate-pak7.sh
+++ builder/generate-pak7.sh
@@ -23,10 +23,20 @@ https://github.com/nunchaku-tech/ComfyUI-nunchaku/raw/refs/heads/main/requiremen
 https://github.com/neverbiasu/ComfyUI-SAM2/raw/refs/heads/main/requirements.txt
 https://github.com/pydn/ComfyUI-to-Python-Extension/raw/refs/heads/main/requirements.txt
 https://github.com/yolain/ComfyUI-Easy-Use/raw/refs/heads/main/requirements.txt
+
+# --- Ajouts manquants ---
+https://github.com/chflame163/ComfyUI_LayerStyle_Advance/raw/refs/heads/main/requirements.txt
+https://github.com/storyicon/comfyui_segment_anything/raw/refs/heads/main/requirements.txt
+https://github.com/lquesada/ComfyUI-Inpaint-CropAndStitch/raw/refs/heads/main/requirements.txt
+https://github.com/sipherxyz/comfyui-art-venture/raw/refs/heads/main/requirements.txt
+https://github.com/balazik/ComfyUI-PuLID-Flux/raw/refs/heads/main/requirements.txt
+https://github.com/lldacing/ComfyUI_PuLID_Flux_ll/raw/refs/heads/main/requirements.txt
+https://github.com/Derfuu/Derfuu_ComfyUI_ModdedNodes/raw/refs/heads/main/requirements.txt
+https://github.com/jamesWalker55/comfyui-various/raw/refs/heads/main/requirements.txt
 )
 
-for line in "${array[@]}";
-    do curl -w "\n" -sSL "${line}" >> pak7.txt
+for line in "${array[@]}"; do
+    curl -fsSL "${line}" >> pak7.txt || true
 done
 
 sed -i '/^#/d' pak7.txt
@@ -37,8 +47,9 @@ sed -i 's/; platform-system=="Windows"//' pak7.txt
 
 sort -ufo pak7.txt pak7.txt
 
-# Remove duplicate items, compare to pak4.txt and pak5.txt
-grep -Fixv -f pak4.txt pak7.txt > temp.txt && mv temp.txt pak7.txt
-grep -Fixv -f pak5.txt pak7.txt > temp.txt && mv temp.txt pak7.txt
+# Remove duplicate items, compare to pak4/5/6
+# grep -Fixv -f pak4.txt pak7.txt > temp.txt && mv temp.txt pak7.txt
+# grep -Fixv -f pak5.txt pak7.txt > temp.txt && mv temp.txt pak7.txt
+# grep -Fixv -f pak6.txt pak7.txt > temp.txt && mv temp.txt pak7.txt
 
 echo "<pak7.txt> generated. Check before use."
